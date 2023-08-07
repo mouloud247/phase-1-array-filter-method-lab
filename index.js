2fa029a8662466a6f1fa6b8253b7edd82384cf2e
Mouloud Mecheter <mouloudmecheter@gmail.com>
2023-08-07 11:48:12 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(list, filterBy) {
+    let b = list.filter(function (name) {
+        return name.toUpperCase() === filterBy.toUpperCase();
+    })
+    return b;
+}
+
+function fuzzyMatch(list, letters) {
+    let b = list.filter(function (name) {
+        return name.startsWith(letters);
+    })
+    return b;
+}
+
+function matchName(list, filterBy) {
+    let b = list.filter(function (driver) {
+        return driver.name === filterBy;
+    })
+    return b;
+}
+
